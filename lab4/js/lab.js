5e60f61605c738b6f709e9ac10fc97f96b5153e8
jluo42 <jluo42@ucsc.edu>
2021-01-27 14:50:26 -0800

    submitting lab5

---
+++ lab4/js/lab.js
@@ -0,0 +1,66 @@
+// Task 1: Pseudocode an Everyday Task
+
+// = Changing your car's oil filter on your own
+// 1. Check what filter your car needs
+  // 1a. Find out what model/make is your car?
+  // 1b. Buy filter for your car
+// 2. Elevate your car's front tires using two ramps.
+  // 2a. Buy if you don't have it!
+// 3. Once your car is elevated safely shut off the engine.
+// 4. Once your car is elevated find out where the oil tank is located.
+  // 4a. If you are unsure check your car's manual or even google it!
+// 5. Once you find where the oil filter is located you might need to unscrew
+   // a panel protecting that section of the car.
+  // 5a. This panel might be blocking you from seeing the actual filter!
+// 6. Once you find the filter get your oil pan ready!
+ // 6a. If you don't have an oil pan you will need to buy one.
+// 7. Before you unscrew the filter be sure to empty out all the dirty oil from the
+   // tank into your oil pan!
+  // 7a. The bolt might be really tight so you might need a special tool!
+  // 7b. The oil also will shoot out so you will need to position the pan
+      // a little away from underneath the hole. However you will eventually need
+      // to position it right under the hole.
+// 8. Once all the dirty oil is done shooting out you should be clear to
+      // remove the oil filter!
+      // 8a. Oil will come out of this also so keep that oil pan ready!
+// 9. Before you screw on your new oil filter put a drop of oil on your finger and
+     // rub it around the side.
+// 10. Screw on new oil filter
+// 11. If necessary screw back on panel covering oil filter.
+// 12. Open your cars hood
+// 13. Find where you can pour new oil into your car.
+    // 13a. Certain cars require certain oil. Check your cars manual or google it
+// 14. Get a funnel and start pouring in the new oil
+    // 14a. Be sure not to pour too much! Or else you will need to repeat the
+          // entire process.
+// 15. To check if you have enough oil take out your car's dip stick and see if
+      // the oil hits between the two holes.
+// 16. If the oil hits between the two holes you know you have enough oil.
+// 17. Some cars you might need to get rid of the warning notification in the system
+    // 17a. Check manual or google if you need to do this.
+// 18. Congrats you are done!
+
+
+// Task 2: Pseudocode a Computer Task.
+// Creating Blackjack/21.
+
+// 1. Shuffle a 52 Card Deck (excluding jokers) before each round.
+// 2. Deal one face down card to each player including the dealer.
+  // 2a. The user's face down card is hidden to all other players besides the user.
+// 3. Deal another face up card to each player including the dealer.
+  // 3a. The faceup card is revealed to all players in the game including the user.
+// 4. Check if the user has a Blackjack.
+  // 4a. If the user is dealt a ace and value card of 10, they win.
+// 5. Check if it is the user's turn to make a decision.
+  // 5a. If it is NOT the user's turn have the computer prompt whose turn it is.
+  // 5b. If it is the user's turn have the computer prompt that it is their turn.
+// 6. When it's the user's turn have the user select a possible remove
+  // 6a. Hit: The user receives another face up card from the dealer.
+    // Check if the user has exceeded a total amount of 21 combining all cards in their hand.
+    // If the total of all cards exceeds 21 the user busts and is out.
+    // If the total of all cards is under 21, the user can either choose to hit or stand.
+  // 6b. Stand: The user holds their total amount of cards until the end and their turn is finished.
+// 7. Check whether all players in the game have either called to stand or bust.
+// 8. When all players are finished, all cards are revealed.
+// 9. Check which player has the highest total by combining the value of all cards in their hands.
+// 10. Player with 21 or the total value closest to 21 is the winner.
---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/*
+ * Author: Johnny Luo and Ryan Masui
+ * Created: January 21 2021
+ * Lab 5
+ * License: Public Domain
+*/
+
+//Define Variables for Our Car
+make = "Audi";
+model = "Rs7";
+color = "Sonic Gray";
+year = "2017";
+
+//calcuate age of the car
+age = 2021 - year;
+
+// output of the variables that were assigned.
+document.writeln("<p><b> Make: </b>" + make + "<br> </p>");
+document.writeln("<p><b> Model: </b>" + model + "<br>");
+document.writeln("<p><b> Color: </b>" + color + "<br>");
+document.writeln("<p><b> Year: </b> " + year + "<br>");
+document.writeln("<p><b> Age: </b>" + age + " years <br></br>");
