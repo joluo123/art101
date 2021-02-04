3fa261b9c17abe03fa98c8040ad16a5c275c0858
jluo42 <jluo42@ucsc.edu>
2021-02-03 17:07:18 -0800

    updating lab 7

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -5,10 +5,14 @@
  * License: Public Domain
 */
 
+//When entering the user will be prompt to enter their input name.
+var enterName = window.prompt("Hello, what is your name?");
+// The input is then stored into this new variable wherethe function will be called to fix.
+var name = findName(enterName);
 //creating a function that takes the user-input and sorts it.
-function findName() {
+function findName(enterName) {
   //prompts a window asking for user name input and stores that input into a variable userName.
-  var userName = window.prompt("Hello, what is your name?");
+  var userName = enterName;
   console.log("userName = ", userName);
   //Splits the user string input into an array.
   var splitNameArray = userName.split(" ");
@@ -16,12 +20,15 @@ function findName() {
   //Sort the array and store that value into a variable.
   var sortNameArray = splitNameArray.sort();
   console.log("sort array = ", sortNameArray);
+  //sortNameArray[0] = sortNameArray[0].charAt(0).toUpperCase();
   //Take the sub-strings in the array and convert it back to a string
   var nameFixed = sortNameArray.join('');
   console.log("nameFixed = ", nameFixed);
+  //Deletes white space.
+  nameFixed.trim();
   //return the fixed array.
   return nameFixed;
 }
 
-//Output
-document.writeln("<p> Your Name is: " , findName(), "</p> </br>");
+//Output with CSS style sheet.
+document.writeln("<p> <div id = divNameContent> Your Name is: " , name, "</p> </div></br>");
