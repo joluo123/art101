94c53c9ae2387056aceeb0e09299b5c9e9a86b12
jluo42 <>
2021-03-03 22:05:38 -0800

    updating lab 15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -9,12 +9,10 @@ $("#activate").click(runAPI);
 
 
 function runAPI() {
-  var input = $("#input").val();
-  var newUrl = "http://numbersapi.com/" + input;
 // Using the core $.ajax() method
 $.ajax({
     // The URL for the request (from the api docs)
-    url: newUrl,
+    url: "https://randomuser.me/api/?format=prettyJSON",
     // The data to send (will be converted to a query string)
     data: {
             // here is where any data required by the api
@@ -23,13 +21,14 @@ $.ajax({
     // Whether this is a POST or GET request
     type: "GET",
     // The type of data we expect back
-    dataType : "",
+    dataType : "json",
     // What do we do when the api call is successful
     //   all the action goes in here
     success: function(data) {
         // do stuff
         console.log("This is working getting the information...");
-        printText(data);
+        var textData = JSON.stringify(data);
+        printText(textData);
         console.log(data);
     },
     // What we do if the api call fails
